55b621377fe44de97c4c58b6db7059ebfabd02b6
Exilic <tobias.engberg@exilic.se>
2021-01-17 15:37:15 +0100

    Counting function

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-const gameArea = document.getElementById("gameArea");
 let goodAttempt = false;
 let playColor = "white";
+let colorsWhite = 0;
+let colorsBlack = 0;
 
-const makeElement = (kind) => document.createElementNS("http://www.w3.org/2000/svg", kind);
-const setSvgAttribute = (target, kind, value) => target.setAttribute(kind, value);
+const makeSvgElement = (kind) => document.createElementNS("http://www.w3.org/2000/svg", kind);
+
+const square = (gridID) => document.getElementById(gridID);
+
+function countColors() {
+  colorsWhite = 0;
+  colorsBlack = 0;
+  for (let yCount = 1; yCount < 9; yCount++) {
+    for (let xCount = 1; xCount < 9; xCount++) {
+      let squareColor = getCurrentColor("y" + yCount + "x" + xCount);
+      if (squareColor == "white") {
+        colorsWhite++;
+      } else if (squareColor == "black") {
+        colorsBlack++;
+      }
+    }
+  }
+}
 
 function flipTurn() {
   if (playColor == "white") {
@@ -14,19 +31,42 @@ function flipTurn() {
 }
 
 function makeCircle(gridID) {
-  let newCircle = makeElement("circle");
+  let newCircle = makeSvgElement("circle");
   for (const [a, b] of [
     ["cx", "35"],
     ["cy", "35"],
     ["r", "28"],
     ["class", playColor],
   ])
-    setSvgAttribute(newCircle, a, b);
-  document.getElementById(gridID).appendChild(newCircle);
+    newCircle.setAttribute(a, b);
+  square(gridID).appendChild(newCircle);
 }
 
-function emptyOrNot(element) {
-  let squareClicked = document.getElementById(element).children.length;
+function changeColor(gridID) {
+  let targetCircle = square(gridID).lastElementChild;
+  let targetColor = targetCircle.getAttribute("class");
+  if (targetColor == "white") {
+    targetCircle.setAttribute("class", "black");
+  } else {
+    targetCircle.setAttribute("class", "white");
+  }
+}
+
+const getCurrentColor = (gridID) => square(gridID).lastElementChild.getAttribute("class");
+
+// Determines if the next brick is an opposite color to the one of the clicked square (can be several steps away)
+function oppositeColor(yNeighbour, xNeighbour) {
+  let gridID = "y" + yNeighbour + "x" + xNeighbour;
+  let currentColor = getCurrentColor(gridID);
+  if (currentColor != playColor) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function emptyOrNot(gridID) {
+  let squareClicked = square(gridID).children.length;
   if (squareClicked == 1) {
     return true;
   } else {
@@ -34,28 +74,17 @@ function emptyOrNot(element) {
   }
 }
 
-function attemptedMove(element) {
-  goodAttempt = false;
-  if (emptyOrNot(element)) {
-    var y = parseInt(String(element).charAt(1));
-    var x = parseInt(String(element).charAt(3));
-
-    for (let yIndex = -1; yIndex < 2; yIndex += 2) {
-      for (let xIndex = -1; xIndex < 2; xIndex++) {
-        coreMechanics(yIndex, xIndex, y, x);
-      }
-    }
-    for (let xIndex = -1; xIndex < 2; xIndex += 2) {
-      let yIndex = 0;
-      coreMechanics(yIndex, xIndex, y, x);
-    }
-    if (goodAttempt) {
-      makeCircle(element);
-      flipTurn();
-    }
+// Service function for discarding potential neighbouring squares that actually fall ourside the gamearea
+function outOfArea(yNeighbour, xNeighbour) {
+  if (yNeighbour == 0 || yNeighbour == 9 || xNeighbour == 0 || xNeighbour == 9) {
+    return true;
+  } else {
+    return false;
   }
 }
 
+// Has all the logic for checking the consequences if a brick is set on the square clicked
+// Consequences can be found in eight directions, based on the horizontal, vertical and diagonal vectors
 function coreMechanics(yIndex, xIndex, y, x) {
   let yNext = y + yIndex;
   let xNext = x + xIndex;
@@ -64,7 +93,6 @@ function coreMechanics(yIndex, xIndex, y, x) {
   let positiveCount = 0;
   while (stillChecking) {
     let possibleID = "y" + yNext + "x" + xNext;
-    console.log(possibleID);
     if (outOfArea(yNext, xNext) || emptyOrNot(possibleID)) {
       stillChecking = false;
     } else {
@@ -87,63 +115,60 @@ function coreMechanics(yIndex, xIndex, y, x) {
   }
 }
 
-function outOfArea(yNeighbour, xNeighbour) {
-  if (yNeighbour == 0 || yNeighbour == 9 || xNeighbour == 0 || xNeighbour == 9) {
-    return true;
-  } else {
-    return false;
+// Checks the square clicked and performs a full turn, if the square is an appropriate one
+function attemptedMove(gridID) {
+  goodAttempt = false;
+  if (emptyOrNot(gridID)) {
+    var y = parseInt(String(gridID).charAt(1));
+    var x = parseInt(String(gridID).charAt(3));
+
+    for (let yIndex = -1; yIndex < 2; yIndex += 2) {
+      for (let xIndex = -1; xIndex < 2; xIndex++) {
+        coreMechanics(yIndex, xIndex, y, x);
+      }
+    }
+    for (let xIndex = -1; xIndex < 2; xIndex += 2) {
+      let yIndex = 0;
+      coreMechanics(yIndex, xIndex, y, x);
+    }
+    if (goodAttempt) {
+      makeCircle(gridID);
+      flipTurn();
+      countColors();
+      console.log(colorsBlack, colorsWhite);
+    }
   }
 }
 
-function oppositeColor(yNeighbour, xNeighbour) {
-  let nextID = "y" + yNeighbour + "x" + xNeighbour;
-  let colorTarget = document.getElementById(nextID).lastElementChild;
-  let currentColor = colorTarget.getAttribute("class");
-  if (currentColor != playColor) {
-    return true;
-  } else {
-    return false;
-  }
-}
+// Lets the current player indicate where to put a new brick
+document.getElementById("gameArea").addEventListener("click", (e) => {
+  attemptedMove(e.path[1].getAttribute("id"));
+});
 
-function changeColor(gridID) {
-  let targetSVG = document.getElementById(gridID);
-  let targetCircle = targetSVG.lastElementChild;
-  let targetColor = targetCircle.getAttribute("class");
-  if (targetColor == "white") {
-    targetCircle.setAttribute("class", "black");
-  } else {
-    targetCircle.setAttribute("class", "white");
-  }
-}
-
+// Sets the static circumstances for beginning the game
 window.addEventListener("load", (e) => {
   for (let yCount = 1; yCount < 9; yCount++) {
     for (let xCount = 1; xCount < 9; xCount++) {
-      let newSVG = makeElement("svg");
+      let newSVG = makeSvgElement("svg");
       for (const [a, b] of [
         ["width", "70"],
         ["height", "70"],
         ["id", "y" + yCount + "x" + xCount],
       ])
-        setSvgAttribute(newSVG, a, b);
-      gameArea.appendChild(newSVG);
+        newSVG.setAttribute(a, b);
+      document.getElementById("gameArea").appendChild(newSVG);
 
-      let newRect = makeElement("rect");
+      let newRect = makeSvgElement("rect");
       for (const [c, d] of [
         ["width", "70"],
         ["height", "70"],
       ])
-        setSvgAttribute(newRect, c, d);
+        newRect.setAttribute(c, d);
       newSVG.appendChild(newRect);
     }
   }
-
   ["y4x4", "y5x5"].forEach(makeCircle);
   flipTurn();
   ["y4x5", "y5x4"].forEach(makeCircle);
-});
-
-gameArea.addEventListener("click", (e) => {
-  attemptedMove(e.path[1].getAttribute("id"));
+  flipTurn();
 });
